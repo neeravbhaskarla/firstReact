a0484229894f054823ac3e563ea4f07f062ad558
Your Name <you@example.com>
2020-05-13 13:10:45 +0530

    Updates

---
+++ precache-manifest.6f686333e2583aa6125d61547a28e8ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdfdf2518b10f5d879c55dc465570d8",
+    "url": "/firstReact/index.html"
+  },
+  {
+    "revision": "a8be6a4e2f837701fe0a",
+    "url": "/firstReact/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a8be6a4e2f837701fe0a",
+    "url": "/firstReact/static/js/2.81410f55.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/firstReact/static/js/2.81410f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf2b8cd581e810930653",
+    "url": "/firstReact/static/js/main.224c09e8.chunk.js"
+  },
+  {
+    "revision": "c1907924da169f45463f",
+    "url": "/firstReact/static/js/runtime-main.71edcf2b.js"
+  }
+]);
